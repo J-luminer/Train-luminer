65e9ca422c2b145218451cd14eb0bd4c9cf51c4b
J-luminer <joelbriannugraha@gmail.com>
2024-10-20 10:54:17 +0700

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navItems = document.querySelectorAll('nav ul li a');
 
-    navItems.forEach((item, index) => {
+    // Menentukan URL halaman aktif
+    const currentPage = window.location.pathname.split('/').pop(); // Mendapatkan nama file halaman
+    navItems.forEach((item) => {
+        const hrefPage = item.getAttribute('href').split('/').pop(); // Mendapatkan halaman dari href
+
+        // Menandai header aktif berdasarkan halaman saat ini
+        if (currentPage === hrefPage) {
+            item.parentElement.classList.add('active');
+        }
+
+        // Menambahkan event hover
         item.addEventListener('mouseenter', () => {
             // Menambahkan efek menjauh ke header lain
             navItems.forEach((otherItem, otherIndex) => {
-                if (otherIndex !== index) {
+                if (otherItem !== item) {
                     otherItem.parentElement.classList.add('move-away');
                 }
             });
 
-            // Menambahkan efek elektron berputar pada header yang di-hover
+            // Menambahkan efek hover pada header yang di-hover
             item.parentElement.classList.add('atom-effect');
         });
 
@@ -20,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 otherItem.parentElement.classList.remove('move-away');
             });
 
-            // Menghapus efek elektron
+            // Menghapus efek hover
             item.parentElement.classList.remove('atom-effect');
         });
     });
